49bf90ed42a6392bf8f4a05f81c37be605602676
Dmitry Mozzherin <dmozzherin@gmail.com>
2020-12-10 07:10:49 -0600

    Close #16 multiple retries before failing

--- io/verifrest/verifrest.go
+++ io/verifrest/verifrest.go
@@ -2,14 +2,15 @@ package verifrest
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 
 	vlib "github.com/gnames/gnlib/domain/entity/verifier"
 	"github.com/gnames/gnlib/encode"
 	"github.com/gnames/gnverify/entity/verifier"
+	log "github.com/sirupsen/logrus"
 )
 
 type verifrest struct {
@@ -32,24 +33,59 @@ func NewVerifier(url string) verifier.Verifier {
 
 // Verify takes names-strings and options and returns verification result.
 func (vr *verifrest) Verify(params vlib.VerifyParams) []vlib.Verification {
+	var attempts int
+	var response []vlib.Verification
 	enc := encode.GNjson{}
 	req, err := enc.Encode(params)
 	if err != nil {
 		log.Printf("Cannot encode names for verification: %s.", err)
 	}
-	r := bytes.NewReader(req)
-	resp, err := vr.client.Post(vr.verifierURL+"verifications", "application/json", r)
+	attempts, err = try(func(int) (bool, error) {
+		r := bytes.NewReader(req)
+		namesRange := fmt.Sprintf("%s-%s", params.NameStrings[0], params.NameStrings[len(params.NameStrings)-1])
+		resp, err := vr.client.Post(vr.verifierURL+"verifications", "application/json", r)
+		if err != nil {
+			log.Warnf("Request is failing for %s.", namesRange)
+			return true, err
+		}
+		respBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Warnf("Body reading is failing for %s.", namesRange)
+			return true, err
+		}
+		response = make([]vlib.Verification, 0)
+		err = enc.Decode(respBytes, &response)
+		if err != nil {
+			log.Warnf("Response decoding is failing for %s.", namesRange)
+			return true, err
+		}
+		return false, nil
+	})
 	if err != nil {
-		log.Printf("Cannot send verification request: %s.", err)
-	}
-	respBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("Cannot read body of response: %s.", err)
-	}
-	var response []vlib.Verification
-	err = enc.Decode(respBytes, &response)
-	if err != nil {
-		log.Printf("Cannot decode verification results: %s.", err)
+		log.Printf("Verification failed for %s-%s after %d attempts.", params.NameStrings[0],
+			params.NameStrings[len(params.NameStrings)-1], attempts)
+		log.Fatal(err)
 	}
 	return response
+}
+
+func try(fn func(int) (bool, error)) (int, error) {
+	var (
+		err        error
+		tryAgain   bool
+		maxRetries int = 5
+		attempt    int = 1
+	)
+	for {
+		tryAgain, err = fn(attempt)
+		if !tryAgain || err == nil {
+			break
+		}
+		attempt++
+		if attempt > maxRetries {
+			return maxRetries, err
+		}
+		time.Sleep(200 * time.Millisecond)
+	}
+	return attempt, err
 }
